e3e74e82ddfcd01e1f44dddcccfb9c76c595ae6e
xcheche <chekwubechukwu23@gmail.com>
2024-04-10 13:17:27 +0100

    creating post via api

--- posts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .import views
-
-urlpatterns = [
-    path('PostList/', views.PostList.as_view(), name='PostList')
-]
--- reddit/urls.py
+++ reddit/urls.py
@@ -1,22 +1,10 @@
-"""reddit URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+#reddit project urls.py
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from posts import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('posts/api', include('posts.urls'))
+    path('admin/',admin.site.urls),
+    path('api/posts',views.PostList.as_view()),
+
 ]
